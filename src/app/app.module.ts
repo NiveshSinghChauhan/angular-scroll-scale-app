f499f086b65e6a445d5c7052f6f028ea1dfae550
NiveshSinghChauhan <nivesh1998@gmail.com>
2018-04-28 15:50:16 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule, Routes} from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { Page1Component } from './page1/page1.component';
+import { Page2Component } from './page2/page2.component';
+
+
+const RoutesArray: Routes = [
+  { path: 'page1' , component: Page1Component },
+  { path: 'page2' , component: Page2Component }
+];
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Page1Component,
+    Page2Component
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(RoutesArray)
   ],
   providers: [],
   bootstrap: [AppComponent]
